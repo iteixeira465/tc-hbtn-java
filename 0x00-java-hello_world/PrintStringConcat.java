1551c52b69bc15aa09392375ad1c9a82811649c1
CIANDT\igabriel <igabriel@ciandt.com>
2022-05-17 14:33:12 -0300

    1

--- 0x00-java-hello_world/PrintStringConcat.java
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -3,6 +3,6 @@ public class PrintStringConcat {
         String texto1 = "Holberton ";
         String texto2 = "School";
         String texto3 = texto1 + texto2;
-        System.out.print("Bem vindo a "+ texto3 +"!");
+        System.out.print("Bem vindo a "+ texto3 +"!\n");
     }
 }
