b81f12cd42afbe0a66eadd93859a328976f767ee
CIANDT\igabriel <igabriel@ciandt.com>
2022-05-23 13:00:47 -0300

    3

---
+++ 0x01/pares/NumerosPares.java
@@ -0,0 +1,12 @@
+public class NumerosPares {
+    public static void main(String[] args) {
+        for (int i=0; i<100; i++){
+
+            if(i==98)
+                System.out.println(i);
+             else if(i%2==0)
+                System.out.print(i + ", ");
+
+            }
+        }
+}
