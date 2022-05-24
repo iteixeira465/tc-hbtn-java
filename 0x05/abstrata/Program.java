74a8198048b95bfd19437e806f156dad22b09e14
CIANDT\igabriel <igabriel@ciandt.com>
2022-05-24 13:43:08 -0300

    Task Herança

--- 0x05/abstrata/Program.java
+++ 0x05/abstrata/Program.java
@@ -1,31 +1,17 @@
-import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
-
 public class Program {
     public static void main(String[] args) {
-        Quadrado quadrado = new Quadrado();
-
-        quadrado.setLado(10);
-
-        if (Retangulo.class.isAssignableFrom(Quadrado.class))
-            System.out.println("Quadrado eh uma subclasse de Retangulo");
-        else
-            System.out.println("Quadrado nao eh uma subclasse de Retangulo");
-
         try {
-            Field campoAltura = Retangulo.class.getDeclaredField("altura");
-            int modifierAltura = campoAltura.getModifiers();
-            System.out.printf("Campo altura eh %s\n", Modifier.toString(modifierAltura));
-
-            Field campoLargura = Retangulo.class.getDeclaredField("altura");
-            int modifierLargura = campoLargura.getModifiers();
-            System.out.printf("Campo largura eh %s\n", Modifier.toString(modifierLargura));
+            if (Modifier.isAbstract(FormaGeometrica.class.getDeclaredMethod("area").getModifiers()))
+                System.out.println("area() eh uma funcao abstrata");
+            else
+                System.out.println("area() nao eh uma funcao abstrata");
         } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            System.out.println("area() nao eh uma funcao");
         }
 
-        System.out.printf("Lado: %.2f\n", quadrado.getLado());
-        System.out.printf("Area: %.2f\n", quadrado.area());
-        System.out.printf(quadrado.toString() + "\n");
+        if (Modifier.isAbstract(FormaGeometrica.class.getModifiers()))
+            System.out.println("FormaGeometrica eh uma classe abstrata");
+        else
+            System.out.println("FormaGeometrica nao eh uma classe abstrata");
     }
 }
