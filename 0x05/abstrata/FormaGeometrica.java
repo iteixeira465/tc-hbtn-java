c3799320cbf503b120b43673d27824c125e85bf3
CIANDT\igabriel <igabriel@ciandt.com>
2022-05-24 13:49:58 -0300

    Task Herança

--- 0x05/abstrata/FormaGeometrica.java
+++ 0x05/abstrata/FormaGeometrica.java
@@ -1,6 +1,4 @@
 public abstract class FormaGeometrica {
 
-    public abstract double area() throws UnsupportedOperationException {
-        throw new UnsupportedOperationException("Funcao area nao implementada");
-    }
+    public abstract double area() throws UnsupportedOperationException;
 }
