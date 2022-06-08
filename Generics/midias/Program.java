783fe372d39cf48e15cb58fb8a3dc38a5618502b
CIANDT\igabriel <igabriel@ciandt.com>
2022-06-08 10:13:21 -0300

    Generics task 1 parte 2

--- Generics/midias/Program.java
+++ Generics/midias/Program.java
@@ -1,16 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+public class Program {
+    public static void main(String[] args) {
+        Livro livro1 = new Livro("Xato", 1);
+        Livro livro2 = new Livro("Coracoes sujos", 3);
+        Livro livro3 = new Livro("Harry Potter e a Pedra Filosofal", 1);
 
-public class Biblioteca<T extends Midia> {
+        Video video1 = new Video("Aprendendo Java Generics", 25);
+        Video video2 = new Video("Apresentando Wildcards Generics em Java", 25);
 
-    ArrayList<T> list = new ArrayList<>();
+        Jornal jornal1 = new Jornal("Folha de Sao Paulo", 130);
+        Jornal jornal2 = new Jornal("Estadao", 113);
 
-    public void adicionarMidia(T midia) {
-        this.list.add(midia);
+        Biblioteca<Livro> bibliotecaLivros = new Biblioteca<>();
+        bibliotecaLivros.adicionarMidia(livro1);
+        bibliotecaLivros.adicionarMidia(livro2);
+        bibliotecaLivros.adicionarMidia(livro3);
+
+        System.out.println(bibliotecaLivros.obterListaMidias());
+
+        Biblioteca<Midia> bibliotecaMidias = new Biblioteca<>();
+        bibliotecaMidias.adicionarMidia(livro1);
+        bibliotecaMidias.adicionarMidia(video1);
+        bibliotecaMidias.adicionarMidia(video2);
+        bibliotecaMidias.adicionarMidia(jornal2);
+
+        System.out.println(bibliotecaMidias.obterListaMidias());
     }
-
-    public ArrayList<T> obterListaMidias() {
-        return list;
-    }
-
 }
