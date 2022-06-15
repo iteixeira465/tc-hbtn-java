7dc6e3fc47ccc81a3560dd0d20f6250e4fa2fac1
CIANDT\igabriel <igabriel@ciandt.com>
2022-06-15 09:20:12 -0300

    streams task 1

---
+++ Streams_Collectors_Optional/preco_minimo/Produto.java
@@ -0,0 +1,38 @@
+import java.util.Locale;
+
+public class Produto {
+    private int codigo;
+    private String nome;
+    private CategoriaProduto categoria;
+    private double preco;
+    
+    public Produto(int codigo, String nome, CategoriaProduto categoria, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.preco = preco;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public CategoriaProduto getCategoria() {
+        return categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.codigo + "] " + this.nome + " " + this.categoria + " R$ " + String.format("%.2f", this.preco);
+    }
+    
+
+}
