981fe73906287d7eeb57b769bcae2f4f467f7a10
stein465 <igorxgabriel@gmail.com>
2022-06-17 09:28:40 -0300

    serialização

--- Serializacao/serializacao/Estudante.java
+++ Serializacao/serializacao/Estudante.java
@@ -1,36 +1,4 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.util.List;
-
-public class SerializarEstudantes<Estudante>{
-    private String nomeArquivo;
-
-    public SerializarEstudantes(String nomeArquivo) {
-        this.nomeArquivo = nomeArquivo;
-    }
-
-    public void serializar(List<Estudante> estudantes) {
-        try (ObjectOutputStream objectOut = new ObjectOutputStream(new FileOutputStream(this.nomeArquivo))){
-            objectOut.writeObject(estudantes);
-        } catch(Exception e) {
-            System.out.println("Nao foi possivel serializar");
-        }
-    }
-
-
-    public List<Estudante> desserializar() {
-        try (ObjectInputStream objectIn = new ObjectInputStream(new FileInputStream(this.nomeArquivo))){
-            @SuppressWarnings("unchecked") List<Estudante> estudantesList = (List<Estudante>) objectIn.readObject();
-            return estudantesList;
-        } catch(Exception e) {
-            System.out.println("Nao foi possivel desserializar");
-            return null;
-        }
-    }
-}import java.io.Serializable;
 public class Estudante implements Serializable{
     private int idade;
     private String nome;
@@ -41,7 +9,7 @@ public class Estudante implements Serializable{
         this.nome = nome;
         this.senha = senha;
     }
-    
+
     public int getIdade() {
         return idade;
     }
@@ -50,7 +18,7 @@ public class Estudante implements Serializable{
     }
     public String getSenha() {
         return senha;
-    }   
+    }
 
     @Override
     public String toString() {
